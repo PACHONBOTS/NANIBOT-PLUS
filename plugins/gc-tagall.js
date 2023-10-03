344089a78c655ae4411f226e680c1cfb6c949747
PACHONBOTS <142656807+PACHONBOTS@users.noreply.github.com>
2023-10-03 15:42:20 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,7 +8,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
   let teks = `*🐒𝗔𝗣𝗔𝗥𝗘𝗖𝗘 𝗠𝗔𝗟𝗗𝗜𝗧𝗢 𝗔𝗗𝗢𝗥𝗡𝗢🐒*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
-    teks += `┣➥🐷 @${mem.id.split('@')[0]}\n`;
+    teks += `┣➥🐢 @${mem.id.split('@')[0]}\n`;
   }
   teks += `*└*🔥𝗡𝗔𝗡𝗜 𝗕𝗢𝗧 𝗗𝗢𝗠𝗜𝗡𝗔 𝗔𝗟𝗩🔥\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
